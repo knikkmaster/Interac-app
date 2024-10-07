4bc128b969543e9c5671c4953e6e0e07320d133b
knikkmaster <31425977+knikkmaster@users.noreply.github.com>
2024-10-07 23:06:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,17 @@ function onSignIn(googleUser) {
     document.getElementById('appContent').style.display = 'block';
 }
 
+// Event listener for the Login button
+document.getElementById('loginButton').addEventListener('click', function() {
+    gapi.load('auth2', function() {
+        gapi.auth2.init({
+            client_id: 'YOUR_CLIENT_ID.apps.googleusercontent.com'
+        }).then(function(auth2) {
+            auth2.signIn().then(onSignIn);
+        });
+    });
+});
+
 // Button event listener for opening the link
 document.getElementById('openLinkButton').addEventListener('click', function() {
     const link = 'https://docs.google.com/forms/d/e/1FAIpQLSdlHG0hKNJb1nt4A1OemUE4yuVVHElbl_qI1I2dKtgIWlMhPg/viewform'; // Change this to your desired link
