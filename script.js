9aa84fcb5457506a1d4d55646defee882837b9ea
knikkmaster <31425977+knikkmaster@users.noreply.github.com>
2024-10-08 00:07:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
-function onLoad() {
-    gapi.load('auth2', function() {
-        gapi.auth2.init({
-            client_id: '751485603242-99q9u0sdfin7046mb2maour9epri8pot.apps.googleusercontent.com'
-        }).then(function(auth2) {
-            // Attach the click handler to your login button
-            document.getElementById('googleLoginButton').addEventListener('click', function() {
-                auth2.signIn().then(onSignIn).catch(function(error) {
-                    console.error('Login failed: ', error);
-                });
-            });
-        });
+document.getElementById('googleLoginButton').addEventListener('click', function() {
+    const auth2 = gapi.auth2.getAuthInstance();
+    auth2.signIn().then(onSignIn).catch(function(error) {
+        console.error('Login failed: ', error);
     });
-}
+});
 
 function onSignIn(googleUser) {
-    // Proceed with user data handling
     const profile = googleUser.getBasicProfile();
-    console.log('User signed in: ' + profile.getName());
+    console.log('ID: ' + profile.getId());
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
 
-    // Show your app content
+    // Hide login screen and show app content after successful login
     document.getElementById('loginScreen').style.display = 'none';
     document.getElementById('appContent').style.display = 'block';
     document.getElementById('logoutButton').style.display = 'block';
 }
+
+// Button event listener for logging out
+document.getElementById('logoutButton').addEventListener('click', logout);
+
+// Button event listener for opening the first link
+document.getElementById('openLinkButton').addEventListener('click', function() {
+    const link = 'https://docs.google.com/forms/d/e/1FAIpQLSdlHG0hKNJb1nt4A1OemUE4yuVVHElbl_qI1I2dKtgIWlMhPg/viewform'; // Change this to your desired link
+    window.open(link, '_blank');
+});
+
+// Button event listener for opening another link
+document.getElementById('anotherLinkButton').addEventListener('click', function() {
+    const anotherLink = 'https://sites.google.com/interacmail.com/newhub/contact-and-locations'; // Change this to your desired link
+    window.open(anotherLink, '_blank');
+});
 
 // Logout function
 function logout() {
     const auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
+
+        // Hide app content and show login screen after logout
         document.getElementById('appContent').style.display = 'none';
         document.getElementById('loginScreen').style.display = 'block';
         document.getElementById('logoutButton').style.display = 'none';
