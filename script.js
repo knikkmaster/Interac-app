036f75f6253c98cfbe94aeb0b53ce23023c72b03
knikkmaster <31425977+knikkmaster@users.noreply.github.com>
2024-10-07 22:04:39 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example JS code that interacts with the page
+document.getElementById('openLinkButton').addEventListener('click', function() {
+    const link = 'https://example.com';
+    window.open(link, '_blank');
+});
